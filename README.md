881946c795bed47dc4b6b12fb0ac767d1236593a
shusuke0812 <shu.ota0812@gmail.com>
2021-05-15 16:06:31 +0900

    [first commit]

---
+++ README.md
@@ -0,0 +1,20 @@
+# Swiftライブラリのサンプル
+
+## 開発環境
+- Xcode version 12.4
+- Swift version 5.0
+- pod version 1.9.3
+
+## 概要
+- Swift/Xcodeを使ったライブラリの作り方とそのサンプル
+
+## 手順
+1. Xcodeより新しく `Framwork` のテンプレートを選んでプロジェクトを作る
+2. 
+
+
+## 参考
+- 
+
+## メモ
+- 
---
+++ SampleSwiftLibrary/SampleSwiftLibrary.h
@@ -0,0 +1,18 @@
+//
+//  SampleSwiftLibrary.h
+//  SampleSwiftLibrary
+//
+//  Created by Shusuke Ota on 2021/5/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleSwiftLibrary.
+FOUNDATION_EXPORT double SampleSwiftLibraryVersionNumber;
+
+//! Project version string for SampleSwiftLibrary.
+FOUNDATION_EXPORT const unsigned char SampleSwiftLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSwiftLibrary/PublicHeader.h>
+
+
