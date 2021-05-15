2eee995f66c0ae679acf869d04ef42e4c70378be
shusuke0812 <shu.ota0812@gmail.com>
2021-05-15 18:28:19 +0900

    [add]ExampleのViewControllerにボタンをタップしてアラートを表示させる処理を追加

--- Example/ViewController/ViewController.swift
+++ Example/ViewController/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import SampleSwiftLibrary
 
 class ViewController: UIViewController {
-
+    /// BaseView
+    private var baseView: BaseView { self.view as! BaseView }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.baseView.delegate = self
     }
-
-
 }
-
+extension ViewController: BaseViewDelegate {
+    func didTapAlertButton() {
+        UIAlertController.showAlertView(vc: self, title: "アラート表示", message: nil, preferredStyle: .alert, okBtnLabel: "OK", cancelBtnLabel: "キャンセル", completion: nil)
+    }
+}
