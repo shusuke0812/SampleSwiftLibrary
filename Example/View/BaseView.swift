1fba1fb0b7f05eaa6b8b0934f6a57c3a1e5d1968
shusuke0812 <shu.ota0812@gmail.com>
2021-05-15 17:15:02 +0900

    [add]Exampleにアラート表示用のボタンを設定する処理をBaseViewに追加

--- Example/View/BaseView.swift
+++ Example/View/BaseView.swift
@@ -7,5 +7,31 @@
 
 import UIKit
 
+protocol BaseViewDelegate: AnyObject {
+    func didTapAlerButton()
+}
+
 class BaseView: UIView {
+    
+    @IBOutlet weak var alertButton: UIButton!
+    
+    weak var delegate: BaseViewDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.initUI()
+    }
+    @IBAction func didTapAlertButton(_ sender: Any) {
+        self.delegate?.didTapAlerButton()
+    }
+}
+// MARK: - Initialized Method
+extension BaseView {
+    private func initUI() {
+        self.alertButton.backgroundColor = .systemRed
+        self.alertButton.setTitle("アラートを表示する", for: .normal)
+        self.alertButton.clipsToBounds = true
+        self.alertButton.layer.cornerRadius = 5
+        self.alertButton.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
