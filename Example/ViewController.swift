8bc838066722c25fe0192900773d8004108d294a
shusuke0812 <shu.ota0812@gmail.com>
2021-05-15 16:50:15 +0900

    [add]ライブラリのデモAppを追加

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Shusuke Ota on 2021/5/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
