eac82faffaa4d39c212e2bc1dfb5058e6c5d592e
shusuke0812 <shu.ota0812@gmail.com>
2021-06-03 17:41:28 +0900

    [add]XCFramework用のプロジェクトを作成

---
+++ OriginalXCFramework/OriginalXCFramework/OriginalXCFramework.h
@@ -0,0 +1,18 @@
+//
+//  OriginalXCFramework.h
+//  OriginalXCFramework
+//
+//  Created by Shusuke Ota on 2021/6/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OriginalXCFramework.
+FOUNDATION_EXPORT double OriginalXCFrameworkVersionNumber;
+
+//! Project version string for OriginalXCFramework.
+FOUNDATION_EXPORT const unsigned char OriginalXCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OriginalXCFramework/PublicHeader.h>
+
+
